81e072f0f975cd69dfe786e80afd6e3ce6ec1d89
lizh63 <lizh63@asiainfo.com>
2024-12-20 20:12:34 +0800

    连接MQTT服务器

--- User/main.c
+++ User/main.c
@@ -7,13 +7,15 @@
 #include "Key.h"
 
 #include <string.h>
+
+#define ESP8266_ONENET_INFO "AT+CIPSTART=\"TCP\",\"mqtts.heclouds.com\",1883\r\n"
+
 int main(void)
 {
 
 	OLED_Init();
 
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); // 中断控制器分组设置
-
 
 	Usart1_Init(115200);
 	Usart2_Init(115200);
@@ -24,6 +26,11 @@ int main(void)
 	LED_Init();
 
 	ESP8266_Init();
+
+	UsartPrintf(USART1, "Connect MQTT Server...\r\n");
+	while (ESP8266_SendCmd(ESP8266_ONENET_INFO, "CONNECT"))
+		Delay_ms(500);
+	UsartPrintf(USART1, "Connect MQTT Server Success\r\n");
 
 	while (1)
 	{
